4b57a833aff01130b01386aa07dc945815114b53
haruymth <108157117+haruymth@users.noreply.github.com>
2023-02-26 17:33:00 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const { token } = process.env;
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages]
+});
+
+//起動確認
+client.once('ready', () => {
+  console.log(`${client.user.tag} Ready`);
+});
+
+//返答
+client.on('messageCreate', message => {
+  if (message.author.bot) {
+    return;
+  }
+  if (Math.round(Math.random() * 100) == 1) {
+    message.reply({
+      content: "草"
+    });
+  }
+  if (Math.round(Math.random() * 10) == 1) {
+    message.reply({
+      content: "草"
+    });
+  }
+});
+
+//Discordへの接続
+client.login(token);
